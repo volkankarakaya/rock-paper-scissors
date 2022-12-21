82817bbf9dca01eb5d24101ea1b6b6321c579019
Alberto-Knox <volkankarakaya2020@gmail.com>
2022-12-21 15:30:30 +0300

    refactor js code and add psuedocode

--- app.js
+++ app.js
@@ -1,72 +1,59 @@
 // get coumputer choice
+const options = [
+  { choice: "Rock", value: 0 },
+  { choice: "Paper", value: 1 },
+  { choice: "Scissors", value: 2 },
+];
+let playerScore = 0;
+let computerScore = 0;
+
+let computerSelection
+let playerSelection
+
+// console.log(computerSelection.value, playerSelection)
+
 function getComputerSelection() {
-  const options = ["rock", "paper", "scissors"];
   let choice = options[Math.floor(Math.random() * options.length)];
   return choice;
-  // console.log(choice)
 }
 
 // get user choice and make it case insensitive
 function getPlayerSelection() {
   let playerSelection = prompt("Enter your selection: ", "");
-  playerSelection = playerSelection.toLowerCase();
   return playerSelection;
 }
 
-// console.log(playerSelection)
+function playRound(playerSelection, computerSelection) {
+  const playerWinCombo = ["0-2", "1-0", "2-1"];
+  let roundWinCombo = `${playerSelection}-${computerSelection.value}`;
 
-// compare user input and computer's choice
-
-function playRound() {
-  let computerSelection = getComputerSelection();
-  let playerSelection = getPlayerSelection();
-  if (computerSelection === playerSelection) {
-    return "draw!";
-  } else if (
-    (computerSelection === "rock" && playerSelection === "scissors") || // all scenarios that computer win
-    (computerSelection === "paper" && playerSelection === "rock") ||
-    (computerSelection === "scissors" && playerSelection === "paper")
-  ) {
-    return `You lose ${computerSelection} beats ${playerSelection}!`;
+  if (playerSelection === computerSelection.value) {
+    playerScore++;
+    computerScore++;
+  } else if (playerWinCombo.includes(roundWinCombo)) {
+    playerScore++;
   } else {
-    return `You won! ${playerSelection} beats ${computerSelection}!`;
+    computerScore++;
+  }
+  console.log(playerScore,computerScore)
+  if(playerScore===5 || computerScore===5){
+    if(playerScore===computerScore){
+      console.log('game is tie!')
+    }else{
+      console.log((playerScore>computerScore)?'player won':'computer won')
+    }
   }
 }
-
-// playRound()
-// console.log(playRound());
-
-// Paper wins against rock
-// rock wins against scissors
-// scissors wins against paper
 
 // keep score of 5 round, return user win if score is bigger else user lose
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (i = 0; i < 5; i++) {
-    result = playRound().substring(0, 8);
-    console.log(result)
-
-    if (result === "draw!") {
-      
-    } else if (result === "You lose") {
-      computerScore = computerScore + 1;
-    } else {
-      playerScore = playerScore + 1;
-    }
-    console.log(`computer score is ${computerScore}, player score is ${playerScore}.`)
-  
-    }
-    if (playerScore === computerScore) {
-        return "Game Draw!";
-      } else if (playerScore > computerScore) {
-        return "Player Won!";
-      } else {
-        return "Computer Won!";
+function playGame(){
+  while(playerScore<5 && computerScore<5){
+    playRound(getPlayerSelection(),getComputerSelection())
+    // console.log(playerScore,computerScore)
   }
-  console.log(result)
+
+  
 }
 
-console.log(game())
+playGame()
